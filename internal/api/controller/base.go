48b1f9c65afcc87e62dee323c9b11be3b646be61
alytsin <alexey.tryapitsiin@gmail.com>
2024-10-02 22:43:11 +0300

    base controller tests

--- internal/api/controller/base.go
+++ internal/api/controller/base.go
@@ -16,6 +16,14 @@ func (c *Base) validateJsonOrSendBadRequest(ctx *gin.Context, v interface{}) boo
 	return true
 }
 
+func (c *Base) validateQueryOrSendBadRequest(ctx *gin.Context, v interface{}) bool {
+	if err := ctx.ShouldBindQuery(v); err != nil {
+		c.doBadRequest(ctx, err)
+		return false
+	}
+	return true
+}
+
 func (c *Base) doBadRequest(ctx *gin.Context, err error) {
 	ctx.JSON(http.StatusBadRequest, ErrorMessage{Error: err.Error()})
 }
--- internal/api/controller/base_test.go
+++ internal/api/controller/base_test.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -17,65 +18,106 @@ type testUriStruct struct {
 	S int `uri:"clientId" binding:"required,gt=0"`
 }
 
+type testQueryStruct struct {
+	Q int `form:"q" binding:"required,gt=0"`
+}
+
 func TestValidateUriOrSendBadRequest(t *testing.T) {
 
-	c := Base{}
-
 	testData := []struct {
+		name   string
 		id     string
 		result bool
 		code   int
 	}{
-		{id: "", result: false, code: http.StatusBadRequest},
-		{id: "0", result: false, code: http.StatusBadRequest},
-		{id: "-1", result: false, code: http.StatusBadRequest},
-		{id: "1", result: true, code: http.StatusOK},
+		{name: "empty id", id: "", result: false, code: http.StatusBadRequest},
+		{name: "zero id", id: "0", result: false, code: http.StatusBadRequest},
+		{name: "negative id", id: "-1", result: false, code: http.StatusBadRequest},
+		{name: "ok", id: "1", result: true, code: http.StatusOK},
 	}
 
 	for _, item := range testData {
-		w := httptest.NewRecorder()
-		ctx, _ := gin.CreateTestContext(w)
+		t.Run(item.name, func(t *testing.T) {
 
-		params := gin.Params{}
-		params = append(params, gin.Param{
-			Key:   "clientId",
-			Value: item.id,
+			c := Base{}
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+
+			params := gin.Params{}
+			params = append(params, gin.Param{
+				Key:   "clientId",
+				Value: item.id,
+			})
+
+			ctx.Params = params
+
+			var req testUriStruct
+			result := c.validateUriOrSendBadRequest(ctx, &req)
+
+			assert.Equal(t, result, item.result)
+			assert.Equal(t, w.Code, item.code)
 		})
-
-		ctx.Params = params
-
-		var req testUriStruct
-		result := c.validateUriOrSendBadRequest(ctx, &req)
-
-		assert.Equal(t, result, item.result)
-		assert.Equal(t, w.Code, item.code)
 	}
 
+}
+
+func TestValidateQueryOrSendBadRequest(t *testing.T) {
+	cases := []struct {
+		name    string
+		request string
+		result  bool
+		code    int
+	}{
+		{name: "zero all", request: "", result: false, code: http.StatusBadRequest},
+		{name: "empty value", request: "q=", result: false, code: http.StatusBadRequest},
+		{name: "string value", request: "q=a", result: false, code: http.StatusBadRequest},
+		{name: "ok", request: "q=1", result: true, code: http.StatusOK},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+
+			ctx.Request, _ = http.NewRequest("GET", fmt.Sprintf("/?%s", tt.request), nil)
+
+			var req testQueryStruct
+			c := Base{}
+
+			result := c.validateQueryOrSendBadRequest(ctx, &req)
+			assert.Equal(t, result, tt.result)
+			assert.Equal(t, w.Code, tt.code)
+		})
+	}
 }
 
 func TestValidateJsonRequestOrBadRequest(t *testing.T) {
 
 	testData := []struct {
+		name    string
 		request string
 		result  bool
 		code    int
 	}{
-		{request: `{}`, result: false, code: http.StatusBadRequest},
-		{request: `{"f":""}`, result: false, code: http.StatusBadRequest},
-		{request: `{"f":"1"}`, result: true, code: http.StatusOK},
+		{name: "empty object", request: `{}`, result: false, code: http.StatusBadRequest},
+		{name: "empty value", request: `{"f":""}`, result: false, code: http.StatusBadRequest},
+		{name: "ok", request: `{"f":"1"}`, result: true, code: http.StatusOK},
 	}
 
 	for _, item := range testData {
-		w := httptest.NewRecorder()
-		ctx, _ := gin.CreateTestContext(w)
+		t.Run(item.name, func(t *testing.T) {
 
-		ctx.Request, _ = http.NewRequest("GET", "/", strings.NewReader(item.request))
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
 
-		var req testJsonStruct
-		c := Base{}
+			ctx.Request, _ = http.NewRequest("POST", "/", strings.NewReader(item.request))
 
-		result := c.validateJsonOrSendBadRequest(ctx, &req)
-		assert.Equal(t, result, item.result)
-		assert.Equal(t, w.Code, item.code)
+			var req testJsonStruct
+			c := Base{}
+
+			result := c.validateJsonOrSendBadRequest(ctx, &req)
+			assert.Equal(t, result, item.result)
+			assert.Equal(t, w.Code, item.code)
+		})
 	}
 }
