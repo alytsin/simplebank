8e6e919022293808b4ee26cdd44059d0c94b6610
alytsin <alexey.tryapitsiin@gmail.com>
2024-10-03 19:21:06 +0300

    fix tests

--- internal/db/user_test.go
+++ internal/db/user_test.go
@@ -24,7 +24,7 @@ func createRandomUser(t *testing.T) *User {
 	require.Equal(t, params.HashedPassword, user.HashedPassword)
 	require.Equal(t, params.FullName, user.FullName)
 	require.Equal(t, params.Email, user.Email)
-	require.Zero(t, user.PasswordChangedAt)
+	require.True(t, user.PasswordChangedAt.IsZero())
 
 	require.NotZero(t, user.CreatedAt)
 
@@ -33,4 +33,35 @@ func createRandomUser(t *testing.T) *User {
 
 func TestCreateUser(t *testing.T) {
 	createRandomUser(t)
+}
+
+func TestDuplicateUser(t *testing.T) {
+	user := createRandomUser(t)
+
+	params := CreateUserParams{
+		Username:       user.Username,
+		HashedPassword: user.HashedPassword,
+		FullName:       user.FullName,
+		Email:          user.Email,
+	}
+
+	dup, err := testQueries.CreateUser(context.Background(), params)
+	require.Error(t, err)
+	require.Empty(t, dup)
+
+}
+
+func TestGetUser(t *testing.T) {
+
+	user := createRandomUser(t)
+
+	found, err := testQueries.GetUser(context.Background(), user.Username)
+	require.Nil(t, err)
+	require.Equal(t, user.Username, found.Username)
+	require.Equal(t, user.HashedPassword, found.HashedPassword)
+	require.Equal(t, user.FullName, found.FullName)
+	require.Equal(t, user.Email, found.Email)
+	require.Equal(t, user.CreatedAt, found.CreatedAt)
+	require.Equal(t, user.PasswordChangedAt, found.PasswordChangedAt)
+
 }
